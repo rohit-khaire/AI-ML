2bd9647cab08931f2d6a59f5c83fe23354ba4b5f
Rohit Khaire <athlius31@gmail.com>
2026-03-28 19:43:17 +0530

    Add GmailToolSpec

---
+++ LlamaIndex/googleToolSpec.py
@@ -0,0 +1,6 @@
+from llama_index.tools.google import GmailToolSpec
+
+tool_spec = GmailToolSpec()
+tool_spec_list = tool_spec.to_tool_list()
+
+[(tool.metadata.name, tool.metadata.description) for tool in tool_spec_list]
